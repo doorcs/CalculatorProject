eca893bf6da1c4390e56f1aafb7f0b0c6cbfd7a4
doorcs <castledoor@pusan.ac.kr>
2025-04-29 20:11:16 +0900

    fix: Main class 이름 수정
    * ClasslessCalculator -> App

--- src/main/java/com/doorcs/calculator/App.java
+++ src/main/java/com/doorcs/calculator/App.java
@@ -2,7 +2,7 @@ package com.doorcs.calculator;
 
 import java.util.Scanner;
 
-public class ClasslessCalculator {
+public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
