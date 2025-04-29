ecb985559e3981c4162c076ef490e01552888d3d
doorcs <castledoor@pusan.ac.kr>
2025-04-29 23:39:42 +0900

    refactor: 연산 결과를 저장하는 필드 캡슐화
    * results 필드를 private final로 수정
    
    * Getter 메서드와 Setter 메서드 구현

--- src/main/java/com/doorcs/calculator/Calculator.java
+++ src/main/java/com/doorcs/calculator/Calculator.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 public class Calculator {
 
-    List<Integer> results = new ArrayList<>();
+    private final List<Integer> results = new ArrayList<>();
 
-    public int calculate(int lhs, int rhs, char op) {
+    public void calculate(int lhs, int rhs, char op) {
         switch (op) {
             case '+':
                 results.add(lhs + rhs);
@@ -21,15 +21,33 @@ public class Calculator {
             case '/':
                 if (rhs == 0) {
                     System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                    return -1;
+                    break;
                 }
                 results.add(lhs / rhs);
                 break;
             default:
                 System.out.println("올바른 사칙연산 기호 (+, -, *, /)를 입력해주세요.");
-                return -1;
         }
+    }
 
-        return results.get(results.size() - 1);
+    public int getResult() {
+        return getNth(results.size() - 1); // Getter 메서드를 활용해 기존 calculate() 메서드의 로직을 구현함
+    }
+
+    public int getNth(int n) {
+        if (n < 0 || n >= results.size()) {
+            System.out.println("잘못된 인덱스입니다.");
+            return -1;
+        }
+        return results.get(n);
+    }
+
+    // 현재 단계에서는 Setter 메서드를 활용할 방법이 떠오르지 않습니다. 데이터 삭제 메서드를 구현한 뒤, main 메서드를 변경할 때 사용해보도록 하겠습니다.
+    public void setNth(int n, int val) {
+        if (n < 0 || n >= results.size()) {
+            System.out.println("잘못된 인덱스입니다.");
+            return;
+        }
+        results.set(n, val);
     }
 }
