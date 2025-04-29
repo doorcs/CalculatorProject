f4b0703edec6d65e5c2fd3663358d9b8c4ce4e42
성문 <castledoor@pusan.ac.kr>
2025-04-30 01:59:37 +0900

    클래스를 적용해 기본적인 연산을 수행할 수 있는 계산기 만들기 (#2)
    * feat: 연산 수행을 담당할 Calculator 클래스 구현
    
    * 연산 결과를 저장하는 컬렉션 타입 필드를 가진 Calculator 클래스 생성
    
    * 사칙연산을 수행한 후, 결과값을 반환하는 `calculate` 메서드 구현
    
    * fix: Main class 이름 수정
    
    * ClasslessCalculator -> App
    
    * refactor: 연산 수행에 Calculator 클래스를 활용
    
    * refactor: 연산 결과를 저장하는 필드 캡슐화
    
    * results 필드를 private final로 수정
    
    * Getter 메서드와 Setter 메서드 구현
    
    * refactor: Calculator 클래스의 메서드 호출 방식 변경
    
    * 연산 결과를 calculate() 메서드 대신 Getter 메서드에서 가져오도록 수정
    
    * feat: 결과를 삭제하는 메서드 구현
    
    * 과제 요구사항에 따라 저장된 연산 결과들 중 가장 먼저 저장된 데이터를 삭제
    
    * refactor: 사칙연산이 아닌 연산도 수행할 수 있도록 변경
    
    * refactor: 수정, 삭제 메서드에 예외 처리 추가
    
    * refactor: main 메서드의 실행 흐름에 예외 처리 추가

---
+++ src/main/java/com/doorcs/calculator/App.java
@@ -0,0 +1,54 @@
+package com.doorcs.calculator;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Calculator calc = new Calculator();
+
+        while (true) {
+            System.out.println("연산을 선택해주세요. 1: 사칙연산, 2: 계산 결과 변경, 3: 가장 먼저 저장된 데이터 삭제");
+            int cmd = sc.nextInt();
+            switch (cmd) {
+                case 1:
+                    System.out.print("첫 번째 숫자를 입력하세요: "); // 출력 문장과 입력 순서는 과제 요구사항에 따른 것
+                    int lhs = sc.nextInt();
+                    System.out.print("두 번째 숫자를 입력하세요: ");
+                    int rhs = sc.nextInt();
+                    System.out.print("사칙연산 기호를 입력하세요: ");
+                    char op = sc.next().charAt(0);
+
+                    calc.calculate(lhs, rhs, op);
+                    int result = calc.getResult();
+                    if (result != -1) { // 연산이 정상적으로 수행되었을 때만 결과 출력
+                        System.out.println("결과: " + result);
+                    }
+                    break;
+                case 2:
+                    System.out.print("변경하려는 인덱스를 입력하세요: ");
+                    int pos = sc.nextInt();
+                    if (calc.getNth(pos) == -1) break; // 인덱스 유효성을 먼저 검사
+                    System.out.print("변경할 값을 입력하세요: ");
+                    int val = sc.nextInt();
+
+                    if (calc.setNth(pos, val) == -1) break;
+                    System.out.println("수정 성공");
+                    break;
+                case 3:
+                    if (calc.removeResult() == -1) break;
+                    System.out.println("삭제 성공");
+                    break;
+                default:
+                    System.out.println("올바른 연산 번호 (1, 2, 3)를 입력해주세요.");
+            }
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String ans = sc.next();
+            if (ans.equals("exit")) {
+                break;
+            }
+        }
+    }
+}
---
+++ src/main/java/com/doorcs/calculator/Calculator.java
@@ -0,0 +1,63 @@
+package com.doorcs.calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+
+    private final List<Integer> results = new ArrayList<>();
+
+    public void calculate(int lhs, int rhs, char op) {
+        switch (op) {
+            case '+':
+                results.add(lhs + rhs);
+                break; // fallthrough 방지!
+            case '-':
+                results.add(lhs - rhs);
+                break;
+            case '*':
+                results.add(lhs * rhs);
+                break;
+            case '/':
+                if (rhs == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    break;
+                }
+                results.add(lhs / rhs);
+                break;
+            default:
+                System.out.println("올바른 사칙연산 기호 (+, -, *, /)를 입력해주세요.");
+        }
+    }
+
+    public int getResult() {
+        return getNth(results.size() - 1); // Getter 메서드를 활용해 기존 calculate() 메서드의 로직을 구현함
+    }
+
+    public int getNth(int n) {
+        if (n < 0 || n >= results.size()) {
+            System.out.println("잘못된 인덱스입니다.");
+            return -1;
+        }
+        return results.get(n);
+    }
+
+    public int setNth(int n, int val) {
+        if (n < 0 || n >= results.size()) {
+            System.out.println("잘못된 인덱스입니다.");
+            return -1;
+        }
+        results.set(n, val);
+        return 0;
+    }
+
+    public int removeResult() {
+        if (results.isEmpty()) {
+            System.out.println("삭제할 결과가 없습니다.");
+            return -1;
+        }
+
+        results.remove(0); // 저장된 연산 결과들 중 가장 먼저 저장된 데이터를 삭제
+        return 0;
+    }
+}
