8e7c2fb21a8a289e9717833551630681da9a374f
doorcs <castledoor@pusan.ac.kr>
2025-05-04 22:58:23 +0900

    refactor: 새로 구현한 메서드를 호출할 수 있도록 변경

--- src/main/java/com/doorcs/calculator/App.java
+++ src/main/java/com/doorcs/calculator/App.java
@@ -9,7 +9,7 @@ public class App {
         Calculator calc = new Calculator();
 
         while (true) {
-            System.out.println("연산을 선택해주세요. 1: 사칙연산, 2: 계산 결과 변경, 3: 가장 먼저 저장된 데이터 삭제");
+            System.out.println("연산을 선택해주세요. 1: 사칙연산, 2: 계산 결과 변경, 3: 가장 먼저 저장된 데이터 삭제, 4: 입력값보다 큰 데이터 출력");
             int cmd = sc.nextInt();
             switch (cmd) {
                 case 1 -> {
@@ -38,9 +38,21 @@ public class App {
                     if (calc.removeResult() == -1) break;
                     System.out.println("삭제 성공");
                 }
+                case 4 -> {
+                    System.out.println("값을 입력하세요: ");
+                    int val = sc.nextInt();
+
+                    var results = calc.getResultsGreaterThan(val);
+                    if (results.isEmpty()) {
+                        System.out.println("입력값보다 큰 데이터가 없습니다.");
+                    } else {
+                        System.out.print("입력값보다 큰 데이터(들)은 다음과 같습니다: ");
+                        for (int elem : results) System.out.print(elem + " ");
+                        System.out.println();
+                    }
+                }
                 default -> System.out.println("올바른 연산 번호 (1, 2, 3)를 입력해주세요.");
             }
-
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String ans = sc.next();
